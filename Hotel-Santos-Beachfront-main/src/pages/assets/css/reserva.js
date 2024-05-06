2c4ab22be266840f6d7e001e83d898e0c5d1c062
RaphaelFNS <142854379+RaphaelFNS@users.noreply.github.com>
2024-05-06 19:47:36 -0300

    Update reserva.js

--- Hotel-Santos-Beachfront-main/src/pages/assets/css/reserva.js
+++ Hotel-Santos-Beachfront-main/src/pages/assets/css/reserva.js
@@ -23,9 +23,9 @@ connection.connect((err) => {
   console.log('Conexão com o banco de dados MySQL estabelecida com sucesso.');
 });
 
-app.post('/tbl_usuarios', (req, res) => {
+app.post('/tbl_reserva', (req, res) => {
   const {nome, cpf,  email, telefone, data_nasc, cep, rua, bairro} = req.body;
-  const query = `INSERT INTO tbl_usuarios (nome, cpf,  email, telefone, data_nasc, cep, rua, bairro) VALUES (?, ?)`;
+  const query = `INSERT INTO tbl_reserva (nome, cpf,  email, telefone, data_nasc, cep, rua, bairro) VALUES (?, ?)`;
   connection.query(query, [nome, cpf,  email, telefone, data_nasc, cep, rua, bairro], (err, result) => {
     if (err) {
       console.error('Erro ao fazer reserva:', err);
