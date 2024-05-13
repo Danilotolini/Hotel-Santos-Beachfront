696f2ae046da79cd7f8df597180e87dd50ad028c
RaphaelFNS <142854379+RaphaelFNS@users.noreply.github.com>
2024-05-13 19:20:48 -0300

    Update cadastro.js

--- Hotel-Santos-Beachfront-main/src/pages/Login/cadastro.js
+++ Hotel-Santos-Beachfront-main/src/pages/Login/cadastro.js
@@ -4,35 +4,41 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'raphael',
-  password: '123',
+  host: 'localhost', 
+  user: 'raphael', 
+  password: '123', 
   database: 'Hotel_Santos_Beachfront'
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    return;
-  }
-  console.log('Conexão com o banco de dados MySQL estabelecida com sucesso.');
-});
-
-function verificarUsuarioCadastrado(email) {
-  let usuariosCadastrados = SELECT * FROM tbl_cadastro
-    for (let i = 0; i < usuariosCadastrados.length; i++) {
-        if (usuariosCadastrados[i].email === email) {
-            return true;
-        }
+function verificarUsuarioCadastrado(email, callback) {
+  
+  const sql = `SELECT COUNT(*) AS total FROM tbl_usuarios WHERE email = ?`;
+  
+  connection.query(sql, [email], (err, results) => {
+    if (err) {
+      return callback(err, null);
     }
-    return false;
+
+    const usuarioCadastrado = results[0].total > 0;
+    callback(null, usuarioCadastrado);
+  });
 }
 
-app.post('/tbl_usuarios', (req, res) => {
+
+const emailNovoUsuario = "novo_usuario@example.com";
+verificarUsuarioCadastrado(emailNovoUsuario, (err, usuarioCadastrado) => {
+  if (err) {
+    console.error("Erro ao verificar usuário:", err);
+    return;
+  }
+
+  if (usuarioCadastrado) {
+    console.log("Usuário já cadastrado.");
+  } else {
+    console.log("Usuário não cadastrado. Pode prosseguir com o cadastro.");
+    
+    app.post('/tbl_usuarios', (req, res) => {
   const { cpf, nome, email, senha} = req.body;
   const query = `INSERT INTO tbl_usuarios (CPF, nome, email, senha) VALUES (?, ?)`;
   connection.query(query, [cpf, nome, email, senha], (err, result) => {
@@ -45,6 +51,9 @@ app.post('/tbl_usuarios', (req, res) => {
     }
   });
 });
+  }
+});
+
 //servidor
 const PORT = 3000;
 app.listen(PORT, () => {
